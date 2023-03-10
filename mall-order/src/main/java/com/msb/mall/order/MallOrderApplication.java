4dde9d861d3b6db2094635796d4dfaa01c526c4d
stevenwu <stevenjiayu1229@gmail.ocm>
2023-03-10 23:59:12 +0800

    第一包雲端-初始化

--- mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
@@ -8,6 +8,7 @@ public class MallOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MallOrderApplication.class, args);
+        System.out.println("sss");
     }
 
 }
